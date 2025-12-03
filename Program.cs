dc0d5355eb7c549078c87ddcf9d850074455d7fc
Krešimir Wittenberg <kresimir.wittenberg@gmail.com>
2025-12-03 15:27:53 +0100

    update

--- Program.cs
+++ Program.cs
@@ -11,43 +11,6 @@ services.AddConfigureServices(configuration, env);
 
 
 
-
-
-
-//// Initialize Ollama API client
-//string[] modelName = ["all-minilm", "tinyllama", "qwen2.5:0.5b"];
-//IChatClient chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), modelName[2]);
-//IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = new OllamaApiClient(new Uri("http://localhost:11434"), modelName[0]);
-
-
-//// Configure SQLite Vector Store
-//string dbName = "vector-store.db";
-//var vectorStorePath = Path.Combine(AppContext.BaseDirectory, dbName);
-//var vectorStoreConnectionString = $"Data Source={vectorStorePath}";
-
-
-//services.AddSqliteVectorStore(_ => vectorStoreConnectionString);
-//services.AddSqliteCollection<string, IngestedChunk>(IngestedChunk.CollectionName, vectorStoreConnectionString);
-
-
-//// Register application services
-//services.AddSingleton<DataIngestor>();
-//services.AddSingleton<SemanticSearch>();
-//services.AddKeyedSingleton("ingestion_directory", new DirectoryInfo(Path.Combine(builder.Environment.WebRootPath, "Data")));
-
-
-//services.AddChatClient(chatClient)
-//        .UseFunctionInvocation() // Important for PDF/Document Search!
-//        .UseLogging();
-
-
-//services.AddEmbeddingGenerator(embeddingGenerator);
-
-
-
-
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
